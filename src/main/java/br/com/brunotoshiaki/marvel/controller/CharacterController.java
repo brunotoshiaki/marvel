41e1e70e17a17d42b061289861accb485dc360ff
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-10-18 22:06:53 -0300

    Adicionando dependencias de teste

--- src/main/java/br/com/brunotoshiaki/marvel/controller/CharacterController.java
+++ src/main/java/br/com/brunotoshiaki/marvel/controller/CharacterController.java
@@ -1,16 +1,17 @@
+
 package br.com.brunotoshiaki.marvel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.brunotoshiaki.marvel.service.CharacterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@RestController
+@Component
 @Tag(name = "CharacterEndpoint")
 @RequestMapping("character")
 public class CharacterController {
@@ -19,7 +20,7 @@ public class CharacterController {
     CharacterService service;
 
     @Operation(summary = "Find all Character")
-    @GetMapping(value = "/v1/characters", produces = { "application/json", "application/xml" })
+    @GetMapping(value = "/v1/characters", produces = {"application/json", "application/xml"})
     public ResponseEntity<String> findAll() {
         return ResponseEntity.ok().body(service.getCharacters());
     }
