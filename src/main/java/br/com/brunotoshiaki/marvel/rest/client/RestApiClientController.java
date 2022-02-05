b9df9f88105fcf977d7fe1e72fcf1e5796ad4133
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2022-02-04 22:16:25 -0300

    Criando teste para o CharacterServic

--- src/main/java/br/com/brunotoshiaki/marvel/rest/client/RestApiClientController.java
+++ src/main/java/br/com/brunotoshiaki/marvel/rest/client/RestApiClientController.java
@@ -1,40 +1,38 @@
 package br.com.brunotoshiaki.marvel.rest.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import br.com.brunotoshiaki.marvel.exeption.MarvelApiExeption;
+import br.com.brunotoshiaki.marvel.utils.Utils;
+import javax.validation.constraints.NotNull;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import br.com.brunotoshiaki.marvel.exeption.MarvelApiExeption;
-import br.com.brunotoshiaki.marvel.utils.Utils;
-
-@Controller
 public class RestApiClientController {
 
-    @Autowired
-    WebClient webClient;
+  private final WebClient webClient;
 
-    public RestApiClientController(WebClient.Builder builder, String usersBaseUrl) {
-        this.webClient = builder.baseUrl(usersBaseUrl).build();
+  public RestApiClientController(WebClient.@NotNull Builder builder, String usersBaseUrl) {
+    this.webClient = builder.baseUrl(usersBaseUrl).build();
+  }
+
+  public String restExecute(String path) {
+    var key = Utils.getKey();
+    try {
+      return webClient
+          .get()
+          .uri(
+              uriBuilder ->
+                  uriBuilder
+                      .path(path)
+                      .queryParam("ts", key.getTs())
+                      .queryParam("apikey", key.getApikey())
+                      .queryParam("hash", key.getHash())
+                      .build())
+          .retrieve()
+          .bodyToMono(String.class)
+          .block();
+
+    } catch (WebClientResponseException we) {
+      throw new MarvelApiExeption(we.getMessage(), we.getRawStatusCode());
     }
-
-    public String restExecute(String path) {
-        var key = Utils.getKey();
-        try {
-            return webClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path(path)
-                            .queryParam("ts", key.getTs())
-                            .queryParam("apikey", key.getApikey())
-                            .queryParam("hash", key.getHash()).build())
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
-
-        } catch (WebClientResponseException we) {
-            throw new MarvelApiExeption(we.getMessage(), we.getRawStatusCode());
-        }
-
-    }
-
+  }
 }
--- src/test/java/br/com/brunotoshiaki/marvel/rest/client/CharacterServiceTest.java
+++ src/test/java/br/com/brunotoshiaki/marvel/rest/client/CharacterServiceTest.java
@@ -2,33 +2,54 @@ package br.com.brunotoshiaki.marvel.rest.client;
 
 import static org.junit.Assert.assertEquals;
 import java.io.IOException;
-import org.junit.Test;
-import org.springframework.web.reactive.function.client.WebClient;
-import br.com.brunotoshiaki.marvel.utils.Utils;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.reactive.function.client.WebClient;
+import br.com.brunotoshiaki.marvel.utils.Utils;
 
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@SpringBootTest
 public class CharacterServiceTest {
-    private static MockWebServer mockWebServer = new MockWebServer();
-    private static RestApiClientController controller =
-            new RestApiClientController(WebClient.builder(),
-                    mockWebServer.url("/v1/public/characters").toString());
 
+  private static MockWebServer mockWebServer;
+  private static RestApiClientController controller;
 
-    @Test
-    public void getCharactersTest() throws InterruptedException, IOException {
+  @Before
+  public void setUp() throws IOException {
+    mockWebServer = new MockWebServer();
+    mockWebServer.start();
+    controller =
+        new RestApiClientController(
+            WebClient.builder(), mockWebServer.url("/v1/public/characters").toString());
+  }
 
-        var json = Utils.readFile("src/test/resources/resourceCharacters.json");
+  @Test
+  public void getCharactersTest() throws IOException {
 
-        MockResponse mockResponse = new MockResponse()
-                .addHeader("Content-Type", "application/json; charset=utf-8")
-                .setBody(json);
+    var json = Utils.readFile("src/test/resources/resourceCharacters.json");
 
-        mockWebServer.enqueue(mockResponse);
+    var mockResponse =
+        new MockResponse()
+            .addHeader("Content-Type", "application/json; charset=utf-8")
+            .setBody(json);
 
-        String result = controller.restExecute("/v1/public/characters");
+    mockWebServer.enqueue(mockResponse);
 
-        assertEquals(json, result);
-    }
+    var result = controller.restExecute("/v1/public/characters");
 
+    assertEquals(json, result);
+  }
+
+  @After
+  public void tearDown() throws IOException {
+    mockWebServer.shutdown();
+  }
 }
